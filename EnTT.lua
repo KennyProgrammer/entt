a4316343f8b85c92b44f281c3367b7483b75d0ae
Danil <kennytuts@gmail.com>
2023-11-29 00:28:40 +0300

    Update EnTT.lua file.

--- EnTT.lua
+++ EnTT.lua
@@ -2,6 +2,9 @@
 -- Premake5 file for build EnTT project.
 -- Copyright (c) 2020-2023 by Danil (Kenny) Dukhovenko, All rights reserved.
 --
+-- Requirement:
+--  - ForceEngine.lua
+--
 
 -- ImGuizmo EnTT Project
 project "EnTT"
@@ -9,8 +12,8 @@ project "EnTT"
 	language      "C++"
 	cppdialect    "C++17"
 	staticruntime "On"
-	targetdir     ("%{ForceDir.BinLib}/" .. BuildDir .. "/%{prj.name}/lib")
-	objdir        ("%{ForceDir.BinLib}/" .. BuildDir .. "/%{prj.name}/obj")
+	targetdir     ("%{ForceDir.BinLib}/" .. BuildDir .. "/%{prj.name}/Lib")
+	objdir        ("%{ForceDir.BinLib}/" .. BuildDir .. "/%{prj.name}/Obj")
 
 	files {
 		"include/**.hpp",
