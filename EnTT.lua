77b26f09f5c12543db4f54d157e5fe25df97f336
KennyProgrammer <kennytuts@gmail.com>
2025-05-22 13:46:56 +0300

    Update ENTT to v3.15.0.

--- EnTT.lua
+++ EnTT.lua
@@ -1,16 +1,30 @@
---
--- Premake5 file for build EnTT project.
--- Copyright (c) 2020-2023 by Danil (Kenny) Dukhovenko, All rights reserved.
---
--- Requirement:
---  - ForceEngine.lua
---
+--/*********************************************************************/
+--/*                                                                   */
+--/*********************************************************************/
+--/*                           Force Engine                            */
+--/*              https://danil-dukhovenko.gitbook.io/force            */
+--/*********************************************************************/
+--/*                                                                   */
+--/* API:  Premake5 file for build EnTT project.                       */
+--/* File: ExampleApplication.lua                                      */
+--/*                                                                   */
+--/* Requirement:                                                      */
+--/*  - ForceEngine.lua                                                */
+--/*                                                                   */
+--/* Copyright (c) 2020-2025 by KennyProgrammer (Danil Dukhovenko),    */
+--/* All rights reserved.                                              */
+--/*                                                                   */
+--/* No parts of this code or any of its contents may be reproduced,   */
+--/* copied, modified or adapted, without the prior written consent of */
+--/* the author, unless otherwise indicated for stand-alone materials. */
+--/*                                                                   */
+--/*********************************************************************/
 
 -- ImGuizmo EnTT Project
 project "EnTT"
 	kind          "StaticLib"
 	language      "C++"
-	cppdialect    "C++17"
+	cppdialect    "C++20"
 	staticruntime "On"
 	targetdir     ("%{ForceDir.BinLib}/" .. BuildDir .. "/%{prj.name}/Lib")
 	objdir        ("%{ForceDir.BinLib}/" .. BuildDir .. "/%{prj.name}/Obj")
